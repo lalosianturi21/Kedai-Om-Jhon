3bb78279ee8e9e5f3d7d8b83b84358295f2da0cd
lalosianturi21 <fulalotio@gmail.com>
2025-01-31 10:49:57 +0700

    perubahan

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,45 +1,63 @@
-import prisma from '@/lib/db';
-import { z } from 'zod';
-import { NextResponse } from 'next/server';
+import prisma from '@/lib/db'
+import { z } from 'zod'
 
 export async function GET(req: Request) {
   try {
-    const url = new URL(req.url);
+    const url = new URL(req.url)
 
     const { limit, page, category } = z
       .object({
-        limit: z.string().default("10"),
-        page: z.string().default("1"),
-        category: z.string().optional(),
+        limit: z.string(),
+        page: z.string(),
+        category: z.string(),
       })
       .parse({
-        limit: url.searchParams.get("limit") ?? "10",
-        page: url.searchParams.get("page") ?? "1",
-        category: url.searchParams.get("category") ?? undefined,
-      });
+        limit: url.searchParams.get('limit'),
+        page: url.searchParams.get('page'),
+        category: url.searchParams.get('category'),
+      })
 
-    const categoryValue = category === "null" ? undefined : category;
+    let result
 
-    const products = await prisma.product.findMany({
-      where: categoryValue ? { categoryId: categoryValue } : {},
-      take: parseInt(limit),
-      skip: (parseInt(page) - 1) * parseInt(limit),
-      orderBy: {
-        createdAt: "desc",
-      },
-      include: {
-        Category: true,
-      },
-    });
+    if (category != 'null') {
+      const products = await prisma.product.findMany({
+        where: {
+          categoryId: category,
+        },
+        take: parseInt(limit),
+        skip: (parseInt(page) - 1) * parseInt(limit),
+        orderBy: {
+          createdAt: 'desc',
+        },
+        include: {
+          Category: true,
+        },
+      })
 
-    return NextResponse.json(products);
+      result = products
+    } else {
+      const products = await prisma.product.findMany({
+        take: parseInt(limit),
+        skip: (parseInt(page) - 1) * parseInt(limit),
+        orderBy: {
+          createdAt: 'desc',
+        },
+        include: {
+          Category: true,
+        },
+      })
+
+      result = products
+    }
+    
+    return Response.json(result)
   } catch (error) {
     if (error instanceof z.ZodError) {
-      return new Response("Invalid request data passed", { status: 422 });
+      return new Response('Invalid request data passed', { status: 422 })
     }
 
-    console.error(error);
+    console.log(error)
 
-    return new Response("Could not fetch more posts", { status: 500 });
+    return new Response('Could not fetch more posts', { status: 500 })
   }
 }
