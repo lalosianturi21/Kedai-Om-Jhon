b0142e0f4b7c9efcbfcbba608b8842ef174a8e3e
lalosianturi21 <fulalotio@gmail.com>
2025-01-31 10:12:09 +0700

    perubahan

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
-import prisma from '@/lib/db'
+import prisma from '@/lib/db';
 import { z } from 'zod';
+import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
   try {
@@ -8,39 +8,38 @@ export async function GET(req: Request) {
 
     const { limit, page, category } = z
       .object({
-        limit: z.string().optional(),
-        page: z.string().optional(),
+        limit: z.string().default("10"),
+        page: z.string().default("1"),
         category: z.string().optional(),
       })
       .parse({
-        limit: url.searchParams.get('limit'),
-        page: url.searchParams.get('page'),
-        category: url.searchParams.get('category'),
+        limit: url.searchParams.get("limit") ?? "10",
+        page: url.searchParams.get("page") ?? "1",
+        category: url.searchParams.get("category") ?? undefined,
       });
 
-    const take = limit ? parseInt(limit) : 10;
-    const skip = page ? (parseInt(page) - 1) * take : 0;
+    const categoryValue = category === "null" ? undefined : category;
 
-    const products = await prisma.product
-      .findMany({
-        where: category && category !== 'null' ? { categoryId: category } : {},
-        take,
-        skip,
-        orderBy: { createdAt: 'desc' },
-        include: { Category: true },
-      })
-      .catch((err) => {
-        console.error('Database Error:', err);
-        throw new Error('Database query failed');
-      });
+    const products = await prisma.product.findMany({
+      where: categoryValue ? { categoryId: categoryValue } : {},
+      take: parseInt(limit),
+      skip: (parseInt(page) - 1) * parseInt(limit),
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        Category: true,
+      },
+    });
 
     return NextResponse.json(products);
   } catch (error) {
     if (error instanceof z.ZodError) {
-      return new Response('Invalid request data passed', { status: 422 });
+      return new Response("Invalid request data passed", { status: 422 });
     }
 
     console.error(error);
-    return new Response('Could not fetch more posts', { status: 500 });
+
+    return new Response("Could not fetch more posts", { status: 500 });
   }
 }
